db7e37725ccd1dd1ffd12580ba62d17c69c5a6fe
emerson-cloud <luiscolomboemerson@gmail.com>
2020-07-25 10:32:22 -0300

    Commit Initial

---
+++ _app/Email.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Notification;
+
+use  PHPMailer \ PHPMailer \ PHPMailer ;
+
+class Email{
+    
+    public function __construct() {
+        $email =  new PHPMailer;
+    }
+
+        public function sendMail()
+            {
+        echo "E-mail enviado!!";
+            }
+}
+
